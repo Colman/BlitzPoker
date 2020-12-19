821a77d6396a2dafa46e7a70dd8f0f64c45abde4
Jaxon Thompson <jcthompsn@gmail.com>
2020-12-19 14:41:42 -0700

    home screen

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -5,10 +5,19 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="app-title">Blitz Poker</div>
+        <img src="/Felt1.jpg" className="app-image" />
+        <img src="/Jack.jpg" className="app-jack app-jack1" />
+        <img src="/Jack.jpg" className="app-jack app-jack2" />
+        <img src="/Jack.jpg" className="app-jack app-jack3" />
+        <img src="/Jack.jpg" className="app-jack app-jack4" />
+        <img src="/Jack.jpg" className="app-jack app-jack5" />
+        <div className="app-cover"></div>
+        <div className="app-menu">
+            <div className="app-title">Blitz Poker</div>
 
-        <div className="app-button">Create Room</div>
-        <div className="app-button">Join Room</div>
+            <button className="app-button app-button-top">Create Room</button>
+            <button className="app-button">Join Room</button>
+        </div>
       </div>
     );
   }
